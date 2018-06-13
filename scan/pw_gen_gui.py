3fbace217955a1d21555ab93c219f202816c977c
Alexleslie <577045156@qq.com>
2018-06-13 19:28:03 +0800

    Update pw_gen_gui.py

--- scan/pw_gen_gui.py
+++ scan/pw_gen_gui.py
@@ -1,9 +1,8 @@
 from tkinter import *
 import tkinter.messagebox as messagebox
 from tkinter import scrolledtext
-import os
 import time
-
+from pw_gen import User
 
 
 
@@ -14,17 +13,19 @@ class Application(Frame):
         self.createWidgets()
 
 
-    def webshell_scan(self):
-     
+    def password_generate(self):
 
-        path = self.input.get()
+        User_dict = {'first_name':self.first_name_i.get(), 'last_name':self.last_name_i.get(),      
+                     'abb_name':self.abb_name_i.get(), 'year':self.year_i.get(), 'mouth':self.mouth_i.get(), 
+                     'day':self.day_i.get(), 'phone':self.phone_i.get(), 'email':self.e_mail_i.get(),
+                     'qq':self.qq_i.get(), 'old_account':self.old_account_i.get()}                                  
        
-       
-       
+        user = User(User_dict)       
+        self.password_list = user.password_generate()
        
         self.content.delete(1.0, END)
 
-        for i in self.bad_list :
+        for i in self.password_list :
             i = i + '\n'
             time.sleep(0.1)
             self.content.insert(END, i)
@@ -32,29 +33,75 @@ class Application(Frame):
         
         self.infolabel['text'] = self.filecount
 
-        messagebox.showinfo('扫描成功', '扫描结束')
+        messagebox.showinfo('生成成功', '生成结束')
 
     def createWidgets(self):
-        self.helloLabel = Label(self, text='A&D-WebShell-Scanner',
+        self.helloLabel = Label(self, text='A&D-社工密码生成器',
                                 font=('Fira', 15), width=20, height=1)
         self.helloLabel.grid(row=1, column=2, padx=1, pady=1)
 
-        self.introduction = Label(self, text='A&D工作室开发的基于机器学习的webshell扫描器，目前只支持PHP',
+        self.introduction = Label(self, text='一款简单的社会工程学密码生成器,后续慢慢改进',
                                   font=('Arial', 10),height=2)
         self.introduction.grid(row=2, column=2)
 
-        self.pathLabel = Label(self, text='路径:')
-        self.pathLabel.grid(row=3,column=1)
-
-        self.input = Entry(self, width=50)
-        self.input.grid(row=3, column=2)
-
-        self.filecount = '扫描文件数: 0 , 发现文件数: 0'
+        self.first_name = Label(self, text='名字')
+        self.first_name.grid(row=3,column=1)
+        self.first_name_i = Text(self, text='')
+        self.first_name_i.grid(row=3, column=2)
+        
+        self.last_name = Label(self, text='姓')
+        self.last_name.grid(row=4, column=1)
+        self.last_name_i = Text(self, text='')
+        self.last_name_i.grid(row=4, column=2)
+        
+        self.abb_name = Label(self, text='缩写')
+        self.abb_name.grid(row=5, column=1)
+        self.abb_name_i= Text(self, text='')
+        self.abb_name_i.grid(row=5, column=2)
+        
+        self.year = Label(self, text='year')
+        self.year.grid(row=6, column=1)
+        self.year_i = Text(self, text='')
+        self.year_i.grid(row=6, column=2)
+        
+        self.mouth = Label(self, text='mouth')
+        self.mouth.grid(row=6, column=1)
+        self.mouth_i = Text(self, text='')
+        self.mouth_i.grid(row=6, column=2)
+        
+        self.day = Label(self, text='day')
+        self.day.grid(row=7, column=1)
+        self.day_i = Text(self, text='')
+        self.day_i.grid(row=7, column=2)
+        
+        self.qq = Label(self, text='qq')
+        self.qq.grid(row=7, column=1)
+        self.qq = Text(self, text='')
+        self.qq_i.grid(row=7, column=2)
+        
+        self.e_mail = Label(self, text='e-mail')
+        self.e_mail.grid(row=8, column=1)
+        self.e_mail_i = Label(self, text='')
+        self.e_mail_i.grid(row=8, column=2)
+        
+        self.phone = Label(self, text='phone')
+        self.phone.grid(row=9, column=1)
+        self.phone_i = Label(self, text='')
+        self.phone_i.grid(row=9, column=2)
+        
+        self.old_account = Label(self, text='old_account')
+        self.old_account.grid(row=10, column=1)
+        self.old_account_i = Label(self, text='')
+        self.old_account_i.grid(row=10, column=2)
+         
+        
+        
+        self.filecount = '生成密码数量: 0'
         self.infolabel = Label(self, text=self.filecount,
-                               font=('Arial', 10), width=30, height=1)
-        self.infolabel.grid(row=4, column=2)
+                               font=('Arial', 10), width=10, height=1)
+        self.infolabel.grid(row=4, column=3)
 
-        self.showText = Button(self, text='开始扫描', command=self.webshell_scan)
+        self.showText = Button(self, text='开始生成', command=self.password_generate)
         self.showText.grid(row=3, column=3)
 
         self.tofile = Button(self, text='保存', command=self.save)
@@ -65,14 +112,14 @@ class Application(Frame):
 
 
     def save(self):
-        with open('result.txt', 'w+') as f:
-            for i in self.bad_list:
+        with open('password.txt', 'w+') as f:
+            for i in self.password_list:
                 f.write(i+'\n')
-        messagebox.showinfo('结果', '保存成功 - 文件名为 result.txt')
+        messagebox.showinfo('结果', '保存成功 - 文件名为 password.txt')
 
 
 if __name__ == '__main__':
     app = Application()
-    app.master.title('A&D-webshell扫描器')
+    app.master.title('A&D-密码生成器')
     app.master.geometry('520x400')
     app.mainloop()
